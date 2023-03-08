29b4cdbe6b3fa48c592f2af4b9328efcf5e28a9f
Tantrums281 <127324493+Tantrums281@users.noreply.github.com>
2023-03-09 01:20:34 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w dero1qyfaxuf6cfqt54zxldpsatamxs6shvm6yq460s28fy20c5jm0kqdjqgkgjnug -r 89.38.99.117 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friend-pool.club:10300 -p rpc;
-    sleep 5;
-done
