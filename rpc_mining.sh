471843e7179be8f664b8ff79e422c3122a712846
Tantrums281 <127324493+Tantrums281@users.noreply.github.com>
2023-03-09 02:28:39 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyfaxuf6cfqt54zxldpsatamxs6shvm6yq460s28fy20c5jm0kqdjqgkgjnug -r 192.168.2.12:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friend-pool.club:10300 -p rpc;
+    ./astrominer -w dero1qyfaxuf6cfqt54zxldpsatamxs6shvm6yq460s28fy20c5jm0kqdjqgkgjnug -r 192.168.2.12:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friendspool.club:10300 -p rpc;
     sleep 5;
 done
